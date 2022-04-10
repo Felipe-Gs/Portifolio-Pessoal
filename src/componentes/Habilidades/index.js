f343bd7303fb893a077cd7fae22b143c3c18be8b
Felipe-Gs <felipegs2905@gmail.com>
2022-04-10 09:57:39 -0300

    atualização do curriculo e nova habilidade

--- src/componentes/Habilidades/index.js
+++ src/componentes/Habilidades/index.js
@@ -16,17 +16,21 @@ export default function Habilidades(){
             <div className='grafico'>
                 <Particulas/>
                 <div className='barra'>
-                    Python<ProgressBar style={{width:'85%'}}  variant='success' now={90}animated label={'90%'}/>  </div>  <br/>
+                    Python<ProgressBar style={{width:'85%'}}  variant='success' now={70}animated label={'70%'}/>  </div>  <br/>
                 <div className='barra'>
                     C<ProgressBar style={{width:'85%'}}  variant='success' now={70}animated label={'70%'}/>  </div> <br/>
                 <div className='barra'>
                     HTML e CSS<ProgressBar style={{width:'85%'}}  variant='success' now={60}animated label={'60%'}/>  </div> <br/>
                 <div className='barra'>
-                    ReactJs<ProgressBar style={{width:'85%'}}  variant='success' now={40}animated label={'40%'}/>
+                    ReactJs<ProgressBar style={{width:'85%'}}  variant='success' now={50}animated label={'50%'}/>
                 </div>
                 <br/>
                 <div className='barra'>
                     TypeScript<ProgressBar style={{width:'85%'}}  variant='success' now={40}animated label={'40%'}/>
+                </div>
+                <br/>
+                <div className='barra'>
+                    JavaScript<ProgressBar style={{width:'85%'}}  variant='success' now={60}animated label={'60%'}/>
                 </div>
                 <br/>
                 <div className='barra'>
